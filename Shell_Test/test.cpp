b67110784eb6345d65c23b59b4ededa08e75783f
SeungjunHa <gkak1121@naver.com>
2024-05-22 15:04:36 +0900

    1. Add Help & Exit TestCase 2. Testable Exit

--- Shell_Test/test.cpp
+++ Shell_Test/test.cpp
@@ -6,22 +6,72 @@
 #include <fstream>
 
 #include "../SSD/iSSD.h"
+#include "../Shell/Shell.cpp"
 
 using namespace std;
 using namespace testing;
 
 class SsdMock : public iSSD
 {
+public:
 	MOCK_METHOD(void, write, (int, string), (override));
 	MOCK_METHOD(void, read, (int), (override));
+private:
 };
 
-class ShellTest : public Test
+class ShellTestFixture : public Test
 {
+public:
 	NiceMock<SsdMock> ssdMock{};
+private:
 };
 
-TEST_F(ShellTest, Test)
+class TestableExitActor : public iExit
 {
+public:
+	void doExit() override 
+	{
+		cout << "Tesable Exit" << endl;
+	}
+private:
+};
 
+TEST_F(ShellTestFixture, HelpCallTest)
+{
+	Shell shell;
+	ostringstream redirectedOutput;
+	ostringstream expectedOutput;
+	streambuf* oldCout;
+
+	oldCout = cout.rdbuf(redirectedOutput.rdbuf());
+	shell.help();
+	cout.rdbuf(oldCout);
+
+	oldCout = cout.rdbuf(expectedOutput.rdbuf());
+	shell.helpMessasge();
+	cout.rdbuf(oldCout);
+
+	EXPECT_EQ(redirectedOutput.str(), expectedOutput.str());
+}
+
+TEST_F(ShellTestFixture, DISABLE_ExitCallTest)
+{
+	Shell shell;
+	TestableExitActor testableExitActor;
+
+	ostringstream redirectedOutput;
+	ostringstream expectedOutput;
+	streambuf* oldCout;
+
+	shell.setExit(&testableExitActor);
+
+	oldCout = cout.rdbuf(redirectedOutput.rdbuf());
+	shell.exit();
+	cout.rdbuf(oldCout);
+
+	oldCout = cout.rdbuf(expectedOutput.rdbuf());
+	testableExitActor.doExit();
+	cout.rdbuf(oldCout);
+
+	EXPECT_EQ(redirectedOutput.str(), expectedOutput.str());
 }
