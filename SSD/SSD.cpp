7511eb2f644d7ce420610de959c979fe5783190d
ckdgh.choi <ckdgh310353@naver.com>
2024-05-22 14:21:08 +0900

    SSD에 file* 추가

--- SSD/SSD.cpp
+++ SSD/SSD.cpp
@@ -3,12 +3,17 @@
 #include <filesystem>
 #include <fstream>
 #include "iSSD.h"
+#include "iFile.h"
 
 using namespace std;
 
 class SSD : public iSSD
 {
 public:
+	SSD(iFile* iFile) : m_file(iFile)
+	{
+	}
+
 	void read(int lba) override
 	{
 
@@ -17,4 +22,7 @@ public:
 	{
 
 	}
+
+private:
+	iFile* m_file;
 };
