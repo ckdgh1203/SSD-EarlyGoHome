034e4b1bba12f578c2502c224df7df048fb8983a
kicheolha <kcha2299@gmail.com>
2024-05-22 15:03:56 +0900

    [feature] fullwrite : input exception tc

--- Shell/Shell.cpp
+++ Shell/Shell.cpp
@@ -1,4 +1,53 @@
+#include "../SSD/iSSD.h"
+#include <string>
+#include <stdexcept>
+
+using namespace std;
+
+class NotIncludedPrefix : public exception
+{
+public:
+	NotIncludedPrefix()
+		: exception("Prefix '0x' should be included!!!")
+	{
+
+	}
+};
+
+class NotAllowedInputData : public exception
+{
+public:
+	NotAllowedInputData()
+		: exception("InputData should be set to Capital characters or numbers")
+	{
+
+	}
+};
+
 class Shell
 {
 public:
+	Shell(iSSD* ssd)
+		: m_Ssd(ssd)
+	{
+
+	}
+
+	void fullwrite(const string inputData)
+	{
+		if (inputData[0] != '0' || inputData[1] != 'x')
+		{
+			throw NotIncludedPrefix();
+		}
+
+		for (int index = 2; index < inputData.length(); index++)
+		{
+			if ((inputData[index] < 'A' || 'F' < inputData[index]) && (inputData[index] < '0' || '9' < inputData[index]))
+			{
+				throw NotAllowedInputData();
+			}
+		}
+	}
+private:
+	iSSD* m_Ssd;
 };
--- Shell_Test/test.cpp
+++ Shell_Test/test.cpp
@@ -1,67 +1,61 @@
-#include <gtest/gtest.h>
-#include <gmock/gmock.h>
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
 #include <string>
 #include <vector>
 #include <filesystem>
 #include <fstream>
 
 #include "../SSD/iSSD.h"
+#include "../Shell/Shell.cpp"
 
 using namespace std;
 using namespace testing;
 
 class SsdMock : public iSSD
 {
+public:
 	MOCK_METHOD(void, write, (int, string), (override));
 	MOCK_METHOD(void, read, (int), (override));
 };
 
 class ShellTest : public Test
 {
+public:
 	NiceMock<SsdMock> ssdMock{};
 };
 
-TEST_F(ShellTest, InvalidCommand)
+TEST_F(ShellTest, FullWrite_NotIncludedPrefixException)
+{
+	Shell shell(&ssdMock);
+	string inputData = "abcd1234";
+
+	EXPECT_THROW(shell.fullwrite(inputData), NotIncludedPrefix);
+}
+
+TEST_F(ShellTest, FullWrite_NotAllowedInputDataException)
+{
+	Shell shell(&ssdMock);
+	string inputData = "0xabcd1234";
+
+	EXPECT_THROW(shell.fullwrite(inputData), NotAllowedInputData);
+}
+
+TEST_F(ShellTest, FullWrite_100TimesSuccessfully)
 {
 
 }
 
-TEST_F(ShellTest, InvalidFormat)
+TEST_F(ShellTest, FullWrite_Under100TimesException)
 {
 
 }
 
-TEST_F(ShellTest, LBAOutOfBound)
+TEST_F(ShellTest, FullRead_100TimesSuccessfully)
 {
 
 }
 
-TEST_F(ShellTest, WriteInvalidData)
-{
-
-}
-
-TEST_F(ShellTest, WriteCommandSuccess)
-{
-
-}
-
-TEST_F(ShellTest, WriteCommandFailure)
-{
-
-}
-
-TEST_F(ShellTest, ReadCommandSuccess)
-{
-
-}
-
-TEST_F(ShellTest, ReadCommandFailure)
-{
-
-}
-
-TEST_F(ShellTest, ExitCommandSuccess)
+TEST_F(ShellTest, FullRead_Under100TimesException)
 {
 
 }
