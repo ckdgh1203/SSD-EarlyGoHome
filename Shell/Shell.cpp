d6e930fa1bc47d73fc2c0035cebc064cb2071ce7
Youngjoo <my.cat.liang@gmail.com>
2024-05-22 15:11:14 +0900

    [feature] add tests for read/write

--- Shell/Shell.cpp
+++ Shell/Shell.cpp
@@ -1,13 +1,26 @@
 #include "../SSD/iSSD.h"
 
+#include <cstdlib>
+
 class Shell
 {
 public:
 	Shell(void) {}
 	Shell(iSSD* ssd) : m_ssd(ssd) {}
+
 	string read(unsigned int lba)
 	{
-		return "Out of Lba";
+		if (99 < lba) return "Out of Lba";
+		m_ssd->read(static_cast<int>(lba));
+		// read result from result.txt
+		return "0x00000000";
+	}
+
+	void write(unsigned int lba, const string& data)
+	{
+		if (99 < lba) return;
+		if (data.size() != 10) return;
+		m_ssd->write(static_cast<int>(lba), data);
 	}
 private:
 	iSSD* m_ssd{};
--- Shell_Test/test.cpp
+++ Shell_Test/test.cpp
@@ -14,6 +14,7 @@ using namespace testing;
 
 class SsdMock : public iSSD
 {
+public:
 	MOCK_METHOD(void, write, (int, string), (override));
 	MOCK_METHOD(void, read, (int), (override));
 };
@@ -25,33 +26,44 @@ public:
 	Shell shell{ &ssdMock };
 };
 
-TEST_F(ShellTest, OutOfLba)
+TEST_F(ShellTest, OutOfLbaRead)
 {
 	constexpr int TEST_LBA = 100;
+	EXPECT_CALL(ssdMock, read(_)).Times(0);
 	EXPECT_EQ("Out of Lba", shell.read(TEST_LBA));
-}
-
-TEST_F(ShellTest, VerifyCommand)
-{
-
-}
-
-TEST_F(ShellTest, InvalidDataFormatRead)
-{
-
 }
 
 TEST_F(ShellTest, ReadSuccess)
 {
+	EXPECT_CALL(ssdMock, read(_)).Times(100);
 
+	for (int lba = 0; lba < 100; lba++)
+	{
+		EXPECT_EQ("0x00000000", shell.read(lba));
+	}
+}
+
+TEST_F(ShellTest, OutOfLbaWrite)
+{
+	constexpr int INVALID_LBA = 100;
+	const string dataZero = "0x00000000";
+	EXPECT_CALL(ssdMock, write(_, _)).Times(0);
+	shell.write(INVALID_LBA, dataZero);
 }
 
 TEST_F(ShellTest, InvalidDataFormatWrite)
 {
+	constexpr int VALID_LBA = 99;
 
+	EXPECT_CALL(ssdMock, write(_, _)).Times(0);
+	shell.write(VALID_LBA, "0x0");
 }
 
 TEST_F(ShellTest, WriteSuccess)
 {
+	constexpr int VALID_LBA = 99;
+	const string dataZero = "0x00000000";
 
+	EXPECT_CALL(ssdMock, write(_, _)).Times(1);
+	shell.write(VALID_LBA, dataZero);
 }
