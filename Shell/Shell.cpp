3cae2157e8bccc217697f146c4b5bcd076b8beb5
Youngjoo <my.cat.liang@gmail.com>
2024-05-22 14:39:54 +0900

    [feature] - add OutOfLba test

--- Shell/Shell.cpp
+++ Shell/Shell.cpp
@@ -1,4 +1,14 @@
+#include "../SSD/iSSD.h"
+
 class Shell
 {
 public:
+	Shell(void) {}
+	Shell(iSSD* ssd) : m_ssd(ssd) {}
+	string read(unsigned int lba)
+	{
+		return "Out of Lba";
+	}
+private:
+	iSSD* m_ssd{};
 };
--- Shell_Test/test.cpp
+++ Shell_Test/test.cpp
@@ -5,7 +5,9 @@
 #include <filesystem>
 #include <fstream>
 
+#include "../Shell/Shell.cpp"
 #include "../SSD/iSSD.h"
+
 
 using namespace std;
 using namespace testing;
@@ -18,10 +20,38 @@ class SsdMock : public iSSD
 
 class ShellTest : public Test
 {
+public:
 	NiceMock<SsdMock> ssdMock{};
+	Shell shell{ &ssdMock };
 };
 
-TEST_F(ShellTest, Test)
+TEST_F(ShellTest, OutOfLba)
+{
+	constexpr int TEST_LBA = 100;
+	EXPECT_EQ("Out of Lba", shell.read(TEST_LBA));
+}
+
+TEST_F(ShellTest, VerifyCommand)
+{
+
+}
+
+TEST_F(ShellTest, InvalidDataFormatRead)
+{
+
+}
+
+TEST_F(ShellTest, ReadSuccess)
+{
+
+}
+
+TEST_F(ShellTest, InvalidDataFormatWrite)
+{
+
+}
+
+TEST_F(ShellTest, WriteSuccess)
 {
 
 }
