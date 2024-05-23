bf0f04de920b448b5ff29e8970598742a962e634
ckdgh.choi <ckdgh310353@naver.com>
2024-05-23 15:13:41 +0900

    file io 일부 구현

--- SSD/File.cpp
+++ SSD/File.cpp
@@ -2,28 +2,84 @@
 #include <vector>
 #include <filesystem>
 #include <fstream>
+#include <iostream>
 #include "iFile.h"
 
-using namespace std; 
+using namespace std;
+
+const string NAND_FILE = "nand.txt";
+const string RESULT_FILE = "result.txt";
+const string DEFAULT_DATA = "0x00000000";
 
 class SSDFile : public iFile
 {
 public:
 	string readFromNANDTxt(int lba) override
 	{
-		return "";
+		ifstream file(NAND_FILE);
+		string ret = DEFAULT_DATA;
+
+		if (!file.is_open())
+		{
+			cout << "read file open fail" << endl;
+			return ret;
+		}
+
+		getLBAData(lba, file, ret);
+		file.close();
+
+		return ret;
 	}
 
-	void writeToNANDTxt(int lba, string data) override
+	void writeToNANDTxt(vector<string> buf) override
 	{
+		ofstream file(NAND_FILE);
+		if (!file.is_open())
+		{
+			cout << "write file open fail" << endl;
+			return;
+		}
+
+		for (const string& updateLine : buf)
+		{
+			file << updateLine << endl;
+		};
+
+		file.close();
 	}
 
 	string readFromResultTxt(int lba) override
 	{
-		return "";
+		ifstream file(RESULT_FILE);
+		string ret = DEFAULT_DATA;
+
+		if (!file.is_open())
+		{
+			cout << "read file open fail" << endl;
+			return ret;
+		}
+
+		getLBAData(lba, file, ret);
+		file.close();
+
+		return ret;
 	}
 
 	void writeToResultTxt(int lba, string data) override
+	{}
+
+private:
+	void getLBAData(int lba, ifstream& file, string& ret)
 	{
+		int targetLine = 0;
+		string buf;
+		while(getline(file, buf))
+		{
+			if (targetLine == lba)
+			{
+				ret = buf;
+			}
+			targetLine++;
+		}
 	}
 };
