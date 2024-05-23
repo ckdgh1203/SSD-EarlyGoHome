b4ce26724301e0cc95ebe30b03cef1a5cab510df
YullKim <yull618@naver.com>
2024-05-23 11:18:09 +0900

    SSD Apply Design Pattern : Strategy Pattern : Command, WriteCommand, ReadCommand class 생성

--- SSD/Command.cpp
+++ SSD/Command.cpp
@@ -12,3 +12,39 @@ public:
 	virtual void executeCommand(int lba, string data) = 0;
 };
 
+class WriteCommand : public Command
+{
+public:
+	WriteCommand(int lba, const string& data)
+		: lba(lba), data(data)
+	{}
+	// Command을(를) 통해 상속됨
+	void executeCommand(int lba) override
+	{}
+
+	void executeCommand(int lba, string data) override
+	{
+		//Write 함수 여기로 이동시키기
+	}
+
+private:
+	int lba;
+	string data;
+};
+
+class ReadCommand : public Command
+{
+public:
+	ReadCommand(int lba)
+		: lba(lba)
+	{}
+	// Command을(를) 통해 상속됨
+	void executeCommand(int lba) override
+	{
+		//Read 함수를 여기로 이동시키기
+	}
+	void executeCommand(int lba, string data) override
+	{}
+private:
+	int lba;
+};
