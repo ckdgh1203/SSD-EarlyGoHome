7dc4a6149e676077a92bbfb19cd87d30662b9269
KimYoonjae <rladbswo1124@gmail.com>
2024-05-27 14:06:05 +0900

    Refactor FileTest

--- SSD/File.cpp
+++ SSD/File.cpp
@@ -7,102 +7,18 @@
 
 using namespace std;
 
-const string NAND_FILE = "Data/nand.txt";
-const string RESULT_FILE = "Data/result.txt";
+const string NAND_FILE = "nand.txt";
+const string RESULT_FILE = "result.txt";
 const string DEFAULT_DATA = "0x00000000";
-const string NAND_FILE_FOR_TEST = "../Data/nand.txt";
-const string RESULT_FILE_FOR_TEST = "../Data/result.txt";
 
 class SSDFile : public iFile
 {
 public:
-	string readFromNANDTxt(int lba) override
+	SSDFile(string file)
 	{
-		ifstream file(NAND_FILE);
-		string ret = DEFAULT_DATA;
-
-		if (!file.is_open())
-		{
-			cout << "read file open fail" << endl;
-			return ret;
-		}
-
-		getLBAData(lba, file, ret);
-		file.close();
-
-		return ret;
+		filePath = file;
 	}
 
-	void writeToNANDTxt(vector<string> buf) override
-	{
-		ofstream file(NAND_FILE);
-		if (!file.is_open())
-		{
-			cout << "write file open fail" << endl;
-			return;
-		}
-
-		for (const string& updateLine : buf)
-		{
-			file << updateLine << endl;
-		};
-
-		file.close();
-	}
-
-	string readFromResultTxt() override
-	{
-		ifstream file(RESULT_FILE);
-		string ret;
-		string temp;
-
-		if (!file.is_open())
-		{
-			cout << "read file open fail" << endl;
-			return ret;
-		}
-
-		getline(file, temp);
-		ret = temp;
-
-		file.close();
-
-		return ret;
-	}
-
-	void writeToResultTxt(string data) override
-	{
-		ofstream file(RESULT_FILE);
-		if (!file.is_open())
-		{
-			cout << "write file open fail" << endl;
-			return;
-		}
-
-		file << data << endl;
-
-		file.close();
-	}
-
-private:
-	void getLBAData(int lba, ifstream& file, string& ret)
-	{
-		int targetLine = 0;
-		string buf;
-		while (getline(file, buf))
-		{
-			if (targetLine == lba)
-			{
-				ret = buf;
-			}
-			targetLine++;
-		}
-	}
-};
-
-class SSDFileTest : public iFile
-{
-public:
 	void initTxtFiles()
 	{
 		vector<string> buf;
@@ -116,7 +32,7 @@ public:
 
 	string readFromNANDTxt(int lba) override
 	{
-		ifstream file(NAND_FILE_FOR_TEST);
+		ifstream file(filePath + NAND_FILE);
 		string ret = DEFAULT_DATA;
 
 		if (!file.is_open())
@@ -133,7 +49,7 @@ public:
 
 	void writeToNANDTxt(vector<string> buf) override
 	{
-		ofstream file(NAND_FILE_FOR_TEST);
+		ofstream file(filePath + NAND_FILE);
 		if (!file.is_open())
 		{
 			cout << "write file open fail" << endl;
@@ -150,7 +66,7 @@ public:
 
 	string readFromResultTxt() override
 	{
-		ifstream file(RESULT_FILE_FOR_TEST);
+		ifstream file(filePath + RESULT_FILE);
 		string ret;
 		string temp;
 
@@ -170,7 +86,7 @@ public:
 
 	void writeToResultTxt(string data) override
 	{
-		ofstream file(RESULT_FILE_FOR_TEST);
+		ofstream file(filePath + RESULT_FILE);
 		if (!file.is_open())
 		{
 			cout << "write file open fail" << endl;
@@ -196,4 +112,6 @@ private:
 			targetLine++;
 		}
 	}
+	
+	string filePath;
 };
